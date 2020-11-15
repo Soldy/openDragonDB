2b4d37d2f6ed2b3db669343008ffff7dee4ce3d9
Soldy <4786022+Soldy@users.noreply.github.com>
2020-11-15 22:11:36 +0000

    super class
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,38 +1,103 @@
 'use strict';
 
 
-
-
-let projectsDB ;
-let viewsDB ;
-let applicationsDB ;
-let modulesDB ;
-let groupsDB ;
-let extensionsDB ;
-let filesDB ;
+const fBase = (require('./db.files.js')).base;
+const gBase = (require('./db.groups.js')).base;
+const mBase = (require('./db.modules.js')).base;
+const pBase = (require('./db.projects.js')).base;
+const aBase = (require('./db.apps.js')).base;
+const vBase = (require('./db.views.js')).base;
+const eBase = (require('./db.extensions.js')).base;
 
 const openDragonDbBase = {
+    /*
+     * @public
+     * @return {object}
+     */
+    this.file=function(){
+        return fDB;
+    }
+    /*
+     * @public
+     * @return {object}
+     */
+    this.group=function(){
+        return gDB;
+    }
+    /*
+     * @public
+     * @return {object}
+     */
+    this.module=function(){
+        return mDB;
+    }
+    /*
+     * @public
+     * @return {object}
+     */
+    this.project=function(){
+        return pDB;
+    }
+    /*
+     * @public
+     * @return {object}
+     */
+    this.app=function(){
+        return aDB;
+    }
+    /*
+     * @public
+     * @return {object}
+     */
+    this.view=function(){
+        return vDB;
+    }
+    /*
+     * @public
+     * @return {object}
+     */
+    this.extension=function(){
+        return eDB;
+    }
+    /*
+     * @private
+     * @var {object}
+     */
+    let fDB = new fBase(this);
+    /*
+     * @private
+     * @var {object}
+     */
+    let gDB = new gBase(this);
+    /*
+     * @private
+     * @var {object}
+     */
+    let mDB = new mBase(this);
+    /*
+     * @private
+     * @var {object}
+     */
+    let pDB = new pBase(this);
+    /*
+     * @private
+     * @var {object}
+     */
+    let vDB = new vBase(this);
+    /*
+     * @private
+     * @var {object}
+     */
+    let aDB = new aBase(this);
+    /*
+     * @private
+     * @var {object}
+     */
+    let eDB = new eBase(this);
+    /*
+     * @private
+     * @var {object}
+     */
     let init = function(){
-        projectsDB = new (require('temprc')).temprc(
-            'db/projects.json'
-        );
-        viewsDB = new (require('temprc')).temprc(
-            'db/views.json'
-        );
-        applicationsDB = new (require('temprc')).temprc(
-            'db/applicationss.json'
-        );
-        modulesDB = new (require('temprc')).temprc(
-            'db/modules.json'
-        );
-        groupsDB = new (require('temprc')).temprc(
-            'db/groups.json'
-        );
-        extensionsDB = new (require('temprc')).temprc(
-            'db/extensions.json'
-        );
-        filesDB = new (require('temprc')).temprc(
-            'db/files.json'
-        );
     }
 }
