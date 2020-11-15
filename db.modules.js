3755197efc34c26b84986deae089cf958d0a982b
Soldy <4786022+Soldy@users.noreply.github.com>
2020-11-15 22:04:18 +0000

    modules basic function
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ db.modules.js
@@ -0,0 +1,81 @@
+'use strict'
+const temprc = (require('temprc')).temprc;
+
+exports.base=function(parentIn){
+    /*
+     * @param {string}
+     * @private
+     * @return {string} || {bool}
+     */
+    let search = function(name){
+        let list = DB.all();
+        for(let i in list)
+            if(list[i].name === name)
+            return i;
+        return false;
+
+    }
+    /*
+     * @param {string}
+     * @private
+     * @return {string} || {bool}
+     */
+    let get = function(id){
+        return DB.get(id);
+    }
+    /*
+     * @param {string}
+     * @private
+     * @return {string} || {bool}
+     */
+    let del = function(id){
+       if(DB.del(id))
+           return true;
+       return false;
+    }
+    /*
+     * @param {string}
+     * @private
+     * @return {string} || {bool}
+     */
+
+    let list = function(){
+        let out = [];
+        let list = DB.all();
+        for(let i in list)
+            out.push(list[i].name);
+        return out;
+    }
+    /*
+     * @param {string}
+     * @private
+     * @return {string} || {bool}
+     */
+    let add =function(name){
+        let id = ((Math.floor(Math.random()*36).toString(36)+
+            DB.size+
+            (Math.floor(Math.random()*36).toString(36));
+        if (search(name) !== false)
+            return false;
+        let packet = ({
+            "serial"      : DB.size, 
+            "id"          : id,
+            "name"        : name, // name
+            "files"       : [],
+            "groups"      : []
+         });
+         DB.add(
+             id,
+             packet
+         );
+         return true;
+    }
+    /*
+     * @private
+     * @var {object}
+    */
+    const DB = new temprc(
+       'od_db/modules.json'
+    );
+    const openDragonDb = parentIn;
+}
