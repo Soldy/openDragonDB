2d2973646a52fbad6e98331e2f838eca49fe545c
Soldy <4786022+Soldy@users.noreply.github.com>
2020-12-26 18:28:41 +0000

    files ttypeCheck
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- db.files.js
+++ db.files.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 const temprc = (require('temprc')).temprc;
 
@@ -10,7 +10,7 @@ exports.base=function(parentIn){
     */
     this.search = function(type, name){
         return search(type, name);
-    }
+    };
 
     /*
      * @public
@@ -18,29 +18,34 @@ exports.base=function(parentIn){
      */
     this.command = function(data){
         if(3 > data.length)
-             return help();
+            return help();
         switch (data[2]) {
-            case 'a':
-            case 'add':
-                return commandAdd(data);
-                break;
-            case 'l':
-            case 'list':
-                return commandList();
-                break;
-            case 'd':
-            case 'det':
-                return commandDel(data);
-                break;
-            case 'g':
-            case 'get':
-                return commandGet(data);
-                break;
-            default:
-                return help();
-                break;
+        case 'a':
+        case 'add':
+            return commandAdd(data);
+        case 'l':
+        case 'list':
+            return commandList();
+        case 'd':
+        case 'det':
+            return commandDel(data);
+        case 'g':
+        case 'get':
+            return commandGet(data);
+        default:
+            return help();
         }
-    }
+    };
+    /*
+     * @param {string}
+     * @private
+     * @return {boolean}
+     */
+    const typeCheck = function (type){
+        if(types.indexOf(type) > -1 )
+            return true;
+        return false;
+    };
     /*
      * @param {array}
      * @private
@@ -48,13 +53,13 @@ exports.base=function(parentIn){
      */
     const commandAdd = function(data){
         if(5 > data.length)
-             return help();
+            return help();
         let id = search(data[3], data[4]);
         if ( id !== false)
-            return (cn+" already exist \n");
+            return (cn+' already exist \n');
         add(data[3], data[4]);
-        return (cn+' '+data[3]+' '+data[4]+" added \n");
-    }
+        return (cn+' '+data[3]+' '+data[4]+' added \n');
+    };
     /*
      * @param {array}
      * @private
@@ -62,17 +67,17 @@ exports.base=function(parentIn){
      */
     const commandGet = function(data){
         if(5 > data.length)
-             return help();
+            return help();
         let id = search(data[4], data[5]);
         if ( id === false)
-            return (cn+" not found \n");
+            return (cn+' not found \n');
         let details = get(id);
         return JSON.stringify(
             details,
             null,
             4
         );
-    }
+    };
     /*
      * @param {array}
      * @private
@@ -80,12 +85,12 @@ exports.base=function(parentIn){
      */
     const commandDel = function(data){
         if(5 > data.length)
-             return help();
+            return help();
         if ( get(data[3], data[4]) === false)
-            return (cn+" not found \n");
+            return (cn+' not found \n');
         del(data[3],data[4]);
-        return (cn+' '+data[3]+' '+data[4]+" deleted \n");
-    }
+        return (cn+' '+data[3]+' '+data[4]+' deleted \n');
+    };
     /*
      * @private
      * @return {string}
@@ -97,13 +102,15 @@ exports.base=function(parentIn){
             4
         );
 
-    }
+    };
     /*
      * @param {string}
      * @private
      * @return {string} || {bool}
      */
-    let search = function(type, name){
+    const search = function(type, name){
+        if(typeCheck(type) === false)
+            return false;
         let list = DB.all();
         for(let i in list)
             if(
@@ -113,73 +120,68 @@ exports.base=function(parentIn){
                 return i;
         return false;
 
-    }
+    };
     /*
      * @param {string}
      * @private
      * @return {string} || {bool}
      */
-    let get = function(id){
+    const get = function(id){
         return DB.get(id);
-    }
+    };
     /*
      * @param {string}
      * @private
      * @return {string} || {bool}
      */
-    let del = function(id){
-       if(DB.del(id))
-           return true;
-       return false;
-    }
+    const del = function(id){
+        if(DB.del(id))
+            return true;
+        return false;
+    };
     /*
      * @param {string}
      * @private
      * @return {string} || {bool}
      */
 
-    let list = function(type){
-        if(typeof type === 'undefined')
+    const list = function(type){
+        if(
+            (typeof type === 'undefined')||
+            (typeCheck(type) === false)
+        )
             type = 'all';
         let out = [];
         let list = DB.all();
         for(let i in list)
             out.push(list[i].name+' '+list[i].type);
         return out;
-    }
+    };
     /*
      * @param {string}
      * @param {string}
      * @private
      * @return {string} || {bool}
      */
-    let add =function (type, name){
-        let id = ((Math.floor(Math.random()*36).toString(36)+
+    const add =function (type, name){
+        if(typeCheck(type) === false)
+            return false;
+        let id = (Math.floor(Math.random()*36).toString(36)+
             DB.size+
-            (Math.floor(Math.random()*36).toString(36));
+            (Math.floor(Math.random()*36).toString(36)));
         let packet = ({
-            "serial"   : DB.size, 
-            "id"       : id,
-            "name"     : name, // name
-            "type"     : type, // type
-            "hash"     : "" // hash
-         });
-         DB.add(
-             id,
-             packet
-         );
-         return true;
-    }
-    /*
-     * @param {string}
-     * @private
-     * @return {boolean}
-     */
-    let typeCheck = function (type){
-        if(types.indexOf(type) > -1 )
-            return true;
-        return false;
-    }
+            'serial'   : DB.size, 
+            'id'       : id,
+            'name'     : name, // name
+            'type'     : type, // type
+            'hash'     : '' // hash
+        });
+        DB.add(
+            id,
+            packet
+        );
+        return true;
+    };
     /*
      * @private
      * @var {array}
@@ -189,7 +191,7 @@ exports.base=function(parentIn){
         'css',
         'svg',
         'js'
-    ]
+    ];
     /*
      * @private
      * @var {object}
@@ -203,7 +205,7 @@ exports.base=function(parentIn){
      */
     const help = function(){
         return ('db '+cn+' help \n');
-    }
+    };
     /*
      * @private
      * @var {object}
@@ -214,4 +216,4 @@ exports.base=function(parentIn){
      * @var {string}
      */
     const cn = 'file';
-}
+};
