78780cf6da449b0744179a217eba569617214a37
Soldy <4786022+Soldy@users.noreply.github.com>
2020-12-26 18:35:49 +0000

    projects
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- db.projects.js
+++ db.projects.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 const temprc = (require('temprc')).temprc;
 
 exports.base=function(parentIn){
@@ -9,7 +9,7 @@ exports.base=function(parentIn){
     */
     this.search = function(name){
         return search(name);
-    }
+    };
 
     /*
      * @public
@@ -17,29 +17,24 @@ exports.base=function(parentIn){
      */
     this.command = function(data){
         if(3 > data.length)
-             return help();
+            return help();
         switch (data[2]) {
-            case 'a':
-            case 'add':
-                return commandAdd(data);
-                break;
-            case 'l':
-            case 'list':
-                return commandList();
-                break;
-            case 'd':
-            case 'det':
-                return commandDel(data);
-                break;
-            case 'g':
-            case 'get':
-                return commandGet(data);
-                break;
-            default:
-                return help();
-                break;
+        case 'a':
+        case 'add':
+            return commandAdd(data);
+        case 'l':
+        case 'list':
+            return commandList();
+        case 'd':
+        case 'det':
+            return commandDel(data);
+        case 'g':
+        case 'get':
+            return commandGet(data);
+        default:
+            return help();
         }
-    }
+    };
     /*
      * @param {array}
      * @private
@@ -47,13 +42,13 @@ exports.base=function(parentIn){
      */
     const commandAdd = function(data){
         if(4 > data.length)
-             return help();
+            return help();
         let id = search(data[3]);
         if ( id !== false)
-            return (cn+" already exist \n");
+            return (cn+' already exist \n');
         add(data[3]);
-        return (cn+' '+data[3]+" added \n");
-    }
+        return (cn+' '+data[3]+' added \n');
+    };
     /*
      * @param {array}
      * @private
@@ -61,17 +56,17 @@ exports.base=function(parentIn){
      */
     const commandGet = function(data){
         if(4 > data.length)
-             return help();
+            return help();
         let id = search(data[3]);
         if ( id === false)
-            return (cn+" not found \n");
+            return (cn+' not found \n');
         let details = get(id);
         return JSON.stringify(
             details,
             null,
             4
         );
-    }
+    };
     /*
      * @param {array}
      * @private
@@ -79,12 +74,12 @@ exports.base=function(parentIn){
      */
     const commandDel = function(data){
         if(4 > data.length)
-             return help();
+            return help();
         if ( get(data[3]) === false)
-            return (cn+" not found \n");
+            return (cn+' not found \n');
         del(data[3]);
-        return (cn+' '+data[3]+" deleted \n");
-    }
+        return (cn+' '+data[3]+' deleted \n');
+    };
     /*
      * @private
      * @return {string}
@@ -95,7 +90,7 @@ exports.base=function(parentIn){
             null,
             4
         );
-    }
+    };
     /*
      * @param {string}
      * @private
@@ -105,10 +100,10 @@ exports.base=function(parentIn){
         let list = DB.all();
         for(let i in list)
             if(list[i].name === name)
-            return i;
+                return i;
         return false;
 
-    }
+    };
     /*
      * @param {string}
      * @private
@@ -116,17 +111,17 @@ exports.base=function(parentIn){
      */
     let get = function(id){
         return DB.get(id);
-    }
+    };
     /*
      * @param {string}
      * @private
      * @return {string} || {bool}
      */
     let del = function(id){
-       if(DB.del(id))
-           return true;
-       return false;
-    }
+        if(DB.del(id))
+            return true;
+        return false;
+    };
     /*
      * @param {string}
      * @private
@@ -139,49 +134,48 @@ exports.base=function(parentIn){
         for(let i in list)
             out.push(list[i].name);
         return out;
-    }
+    };
     /*
      * @param {string}
      * @private
      * @return {string} || {bool}
      */
     let add =function(name){
-        let id = ((Math.floor(Math.random()*36).toString(36)+
+        let id = (Math.floor(Math.random()*36).toString(36)+
             DB.size+
-            (Math.floor(Math.random()*36).toString(36));
+            (Math.floor(Math.random()*36).toString(36)));
         if (search(name) !== false)
             return false;
         let packet = ({
-            "serial"       : DB.size, 
-            "id"           : id,
-            "name"         : name, // name
-            "files"        : [],
-            "groups"       : [],
-            "modules"      : [],
-            "extensions"   : [],
-            "applications" : []
-         });
-         DB.add(
-             id,
-             packet
-         );
-         return true;
-    }
+            'serial'       : DB.size, 
+            'id'           : id,
+            'name'         : name, // name
+            'files'        : [],
+            'groups'       : [],
+            'modules'      : [],
+            'extensions'   : [],
+            'applications' : []
+        });
+        DB.add(
+            id,
+            packet
+        );
+        return true;
+    };
     /*
      * @private
      * @var {object}
     */
     const DB = new temprc(
-            'od_db/projects.json'
-        );
-    }
+        'od_db/projects.json'
+    );
     /*
      * @private
      * @return {string}
      */
     const help = function(){
         return ('db '+cn+' help \n');
-    }
+    };
     /*
      * @private
      * @var {object}
@@ -192,4 +186,4 @@ exports.base=function(parentIn){
      * @var {string}
      */
     const cn = 'project';
-}
+};
